43587edf43bf735507c1d1ccca855ba131e04f5b
hubgit5487301 <48882669+hubgit5487301@users.noreply.github.com>
2024-11-16 17:24:39 +0530

    Update login.js

--- script/login.js
+++ script/login.js
@@ -1,16 +1,30 @@
-let registerbutton = document.getElementById("registerbutton");
 let leftbox = document.getElementById("leftbox");
-let login = document.getElementById("loginbuttonreg");
+let registerbutton = document.getElementById("registerbutton")
+let forgotpasswordbutton = document.getElementById("forgotpasswordbutton");
+
 let middlebox = document.getElementById("middlebox");
+let loginbuttonreg = document.getElementById("loginbuttonreg");
+
+let centerbox = document.getElementById("centerbox");
+let backbutton = document.getElementById("backbutton");
+
 
 registerbutton.addEventListener("click", ()=>{
   leftbox.style.display="none";
   middlebox.style.display="flex";
 });
 
-
-
-login.addEventListener("click", ()=>{
+loginbuttonreg.addEventListener("click", ()=>{
   middlebox.style.display="none";
+  leftbox.style.display="flex";
+});
+
+forgotpasswordbutton.addEventListener("click", ()=>{
+  centerbox.style.display="flex";
+  leftbox.style.display="none";
+});
+
+backbutton.addEventListener("click", ()=>{
+  centerbox.style.display="none";
   leftbox.style.display="flex";
 });
