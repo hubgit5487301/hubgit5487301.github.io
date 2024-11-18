eff11b6236961c09f07f67682dda225ea4549683
hubgit5487301 <48882669+hubgit5487301@users.noreply.github.com>
2024-11-18 18:03:23 +0530

    Add files via upload

---
+++ script/nav-bar.js
@@ -0,0 +1,5 @@
+let homeicon = document.getElementById("homeicon");
+let friendsicon = document.getElementById("homeicon");
+let eventsicon = document.getElementById("homeicon");
+let jobicon = document.getElementById("homeicon");
+let contactusicon = document.getElementById("homeicon");
